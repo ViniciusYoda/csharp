a8d8a72c20557443a66614697d0f02e9f8f66289
Vinícius Yoda <vyoda4604@gmail.com>
2026-01-15 19:15:04 -0300

    up

---
+++ aula17/aula17.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Aula17
+{
+    static void Main()
+    {
+        int[] n=new int[5];
+        int[] num=new int[3]{55,77,99};
+        int[] nume={66,22,88};
+
+        n[0] = 111;
+        n[1] = 222;
+        n[2] = 333;
+        n[3] = 444;
+        n[4] = 555;
+
+        Console.WriteLine(nume[0]);
+    }
+}
