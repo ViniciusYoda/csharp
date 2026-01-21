b955b0dd559bf6b90c1760963ea43eb34e5c1b96
Vinícius Yoda <vyoda4604@gmail.com>
2026-01-21 19:21:51 -0300

    xs

---
+++ aula27/aula27.cs
@@ -0,0 +1,28 @@
+using System;
+
+class Aula27
+{
+    static void Main()
+    {
+        Soma(3, 6, 5);
+    }
+
+    static void Soma(params int[]n)
+    {
+        int res=0;
+        if (n.Length < 1)
+        {
+            Console.WriteLine("Não existe valores a serem somados");
+        } else if (n.Length < 2)
+        {
+            Console.WriteLine("Valores insuficientes para soma");
+        } else
+        {
+            for (int i=0; i < n.Length; i++)
+            {
+                res+=n[i];
+            }
+            Console.WriteLine("A soma dos valores é  {0}", res);
+        }
+    }
+}
