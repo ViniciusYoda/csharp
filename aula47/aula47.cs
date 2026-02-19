88f3f7d76112b9037e63ab0ba63e2f15f55d2353
Vinícius Yoda <vyoda4604@gmail.com>
2026-02-19 19:25:15 -0300

    up

---
+++ aula47/aula47.cs
@@ -0,0 +1,38 @@
+using System;
+
+class Calc
+{
+    public int soma(params int[]n)
+    {
+        int s = 0;
+        for(int i=0; i < n.Length; i++)
+        {
+            s+=n[i];
+        }
+        return s;
+    }
+
+    public double soma(params double[] n)
+    {
+        double s = 0;
+        for(double i=0; i < n.Length; i++)
+        {
+            s+=n[i];
+        }
+        return s;
+    }
+}
+
+class Aula47
+{
+    static void Main()
+    {
+        int res;
+
+        Calc calc = new Calc();
+
+        res = calc.soma(10.2,5.4);
+
+        Console.WriteLine(res);
+    }
+}
