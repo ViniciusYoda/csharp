116368b422c6ac5b422b5636e77164c0dc1ee12c
Vinícius Yoda <vyoda4604@gmail.com>
2026-04-15 19:04:26 -0300

    up

---
+++ roch/Aula02/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Aula02;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Comentário de uma linha
+        string myName = "Roch";
+        Console.WriteLine("Hello, " + myName + "!");
+
+        int number = 10;
+        Console.WriteLine("The number is: " + number);
+
+        double pi = 3.14;
+        Console.WriteLine("The value of pi is: " + pi);
+
+        char letter = 'A';
+        Console.WriteLine("The letter is: " + letter);
+
+        bool isCSharpFun = true;
+        Console.WriteLine("Is C# fun? " + isCSharpFun);
+    }
+}
---
+++ roch/DotnetVsCode/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace DotnetVsCode;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
