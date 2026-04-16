d77649da6789b646b8961dd4eeddaf0e42924911
Vinícius Yoda <vyoda4604@gmail.com>
2026-04-16 19:03:23 -0300

    up

---
+++ roch/Aula04/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Aula04;
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("--------CADASTRO DE USUÁRIO--------");
+
+        Console.Write("Digite seu nome: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Digite sua idade: ");
+        int age = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("----------------------------");
+
+        Console.WriteLine("Nome: " + name);
+        Console.WriteLine("Idade: " + age);
+    }
+}
